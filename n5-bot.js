858dc8a0ce9f48cf724b88d44019730b8b381bda
Liam Shaw <lgshawry@gmail.com>
2017-08-17 21:00:09 +1000

    fixed player name and title

--- n5-bot.js
+++ n5-bot.js
@@ -8,6 +8,7 @@ var bot = new Discord.Client();
 var wow = bnet.wow;
 var i = 5;
 
+const charImage = "http://render-us.worldofwarcraft.com/character/";
 const sumValues = obj => Object.values(obj).reduce((a, b) => a + b)
 
 bot.on("message", message =>
@@ -66,35 +67,32 @@ bot.on("message", message =>
         if(info.status == "nok"){
           message.channel.send("Character not found - try again");
         } else {
-        imgURL = charImage + info.thumbnail;
-        playerTitles = info.titles;
-        console.log(info.name);
-        var artifactRank = artifactWeapon(info);
-        var activePlayerTitle = searchObj(playerTitles, true)[0].name.replace(/([%s])/g, "");
-        console.log(activePlayerTitle);
-        message.channel.send({embed: {
-          color: classLookup[info.class].color,
-          author: {
-            name: info.name + activePlayerTitle,
-            url: `https://worldofwarcraft.com/en-us/character/${region}/${charName}`,
-          },
-          thumbnail: {
-            url: imgURL
-          },
-          fields: [{
-            name: info.talents[0].spec.name + ' ' + classLookup[info.class].name,
-            value: info.items.averageItemLevel +  ' iLvl - Artifact Rank: ' + artifactRank,
-          },
-          {
-            name: "Armory",
-            value: "NYI",
-          },
-          {
-            name: "Raid progression",
-            value: "NYI",
-          },
-        ],
-        }});
+          imgURL = charImage + info.thumbnail;
+          playerTitles = info.titles;
+          var artifactRank = artifactWeapon(info);
+          message.channel.send({embed: {
+            color: classLookup[info.class].color,
+            author: {
+              name: checkTitleExists(info.name, playerTitles),
+              url: `https://worldofwarcraft.com/en-us/character/${region}/${charName}`,
+            },
+            thumbnail: {
+              url: imgURL
+            },
+            fields: [{
+              name: `${info.talents[0].spec.name} ${classLookup[info.class].name}`,
+              value: `${info.items.averageItemLevel} iLvl - Artifact Rank: ${artifactRank}`,
+            },
+            {
+              name: "Armory",
+              value: "NYI",
+            },
+            {
+              name: "Raid progression",
+              value: "NYI",
+            },
+          ],
+          }});
         };
       });
     } else {
@@ -122,35 +120,31 @@ bot.on("message", message =>
 bot.login("MjE4NzA1NjM1Njk5NTg5MTIx.CqHFCg.sTI60tL_bHvFfO-ksa0biM8-rms");
 var apikey = "qnehqjeq658chy2ak9qqkp7q4ft9gmu4";
 
-var charImage = "http://render-us.worldofwarcraft.com/character/";
-
-
-
 function getCharData(charName, region, callback)  {
   request(`https://us.api.battle.net/wow/character/${region}/${charName}?fields=items,titles,talents,progression&locale=en_US&apikey=${apikey}`, function (error, response, result) {
     if (!error && response.statusCode == 200) {
-    var info = JSON.parse(result);
-    console.log(info);
-    callback(info);
-  } else {
-    var info = JSON.parse(result);
-    console.log(info);
-    callback(info);
-  };
+      var info = JSON.parse(result);
+      console.log(info);
+      callback(info);
+    } else {
+      var info = JSON.parse(result);
+      console.log(info);
+      callback(info);
+    };
   });
 }
 
 function getRealmStatus(callback)  {
-  request('https://us.api.battle.net/wow/realm/status?realms=caelestrasz,barthilas&locale=en_US&apikey='+apikey, function (error, response, result) {
+  request(`https://us.api.battle.net/wow/realm/status?realms=caelestrasz,barthilas&locale=en_US&apikey=${apikey}`, function (error, response, result) {
     if (!error && response.statusCode == 200) {
-    var info = JSON.parse(result);
-    console.log(info);
-    callback(info);
-  } else {
-    var info = JSON.parse(result);
-    console.log(info);
-    callback(info);
-  };
+      var info = JSON.parse(result);
+      console.log(info);
+      callback(info);
+    } else {
+      var info = JSON.parse(result);
+      console.log(info);
+      callback(info);
+    };
   });
 }
 
@@ -161,9 +155,20 @@ function searchObj (obj, value) {
   return result;
 }
 
+function checkTitleExists(player, data) {
+  if (searchObj(data, true).length > 0) {
+    var activePlayerTitle = searchObj(playerTitles, true)[0].name.replace(/(%s)/g, player);
+    console.log(activePlayerTitle);
+    return activePlayerTitle;
+  } else {
+    console.log(player);
+    return player;
+  };
+};
+
 function artifactWeapon(info) {
   if (info.items.mainHand.artifactTraits.length < 1) {
-    return
+    return;
   }
   var result = (sumValues(info.items.mainHand.artifactTraits.map(function(a) { return a.rank;})))-3;
   return result;

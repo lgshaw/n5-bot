dd45e2e7a6d725bc34abade7246af083bd20d096
Liam Shaw <lshaw@seek.com.au>
2018-08-08 14:59:00 +1000

    bfa countdown timer

--- n5-bot.js
+++ n5-bot.js
@@ -171,6 +171,13 @@ client.on("message", message =>
     });
   }
 
+  if(input === "!BFA")
+  {
+    countDownToBFA('08/014/2018 08:00:00 AM', function(timer) {
+        message.channel.send(`${timer} until Battle For Azeroth launches` );
+    });
+  }
+
   if(input === "!HELP")
   {
     message.channel.send('!char character-name server-name');
@@ -178,6 +185,38 @@ client.on("message", message =>
 });
 
 
+const countDownToBFA = (endDate, callback) => {
+  let days, hours, minutes, seconds;
+  
+  endDate = new Date(endDate).getTime();
+
+  calculate();
+  
+  function calculate() {
+    let startDate = new Date();
+    startDate = startDate.getTime();
+    
+    let timeRemaining = parseInt((endDate - startDate) / 1000);
+    
+    if (timeRemaining >= 0) {
+      days = parseInt(timeRemaining / 86400);
+      timeRemaining = (timeRemaining % 86400);
+      
+      hours = parseInt(timeRemaining / 3600);
+      timeRemaining = (timeRemaining % 3600);
+      
+      minutes = parseInt(timeRemaining / 60);
+      timeRemaining = (timeRemaining % 60);
+      
+      seconds = parseInt(timeRemaining);
+
+      return callback(`${days} days, ${hours} hrs, ${minutes}mins`);
+      
+    } else {
+      return 'It\'s released you silly sausage. Go and play!';
+    }
+  }
+}
 
 const getCharData = ( charName, region ) =>  {
   return axios(`https://us.api.battle.net/wow/character/${region}/${charName}?fields=items,titles,talents,progression,achievements,stats,statistics&locale=en_US&apikey=${apiKey}`)
---
+++ test-bot.js
@@ -0,0 +1,36 @@
+const countDownToBFA = (endDate, callback) => {
+    let days, hours, minutes, seconds;
+    
+    endDate = new Date(endDate).getTime();
+
+    calculate();
+    
+    function calculate() {
+      let startDate = new Date();
+      startDate = startDate.getTime();
+      
+      let timeRemaining = parseInt((endDate - startDate) / 1000);
+      
+      if (timeRemaining >= 0) {
+        days = parseInt(timeRemaining / 86400);
+        timeRemaining = (timeRemaining % 86400);
+        
+        hours = parseInt(timeRemaining / 3600);
+        timeRemaining = (timeRemaining % 3600);
+        
+        minutes = parseInt(timeRemaining / 60);
+        timeRemaining = (timeRemaining % 60);
+        
+        seconds = parseInt(timeRemaining);
+
+        return callback(`${days} days, ${hours} hrs, ${minutes}mins`);
+        
+      } else {
+        return 'It\'s released you silly sausage. Go and play!';
+      }
+    }
+  }
+
+  countDownToBFA('08/014/2018 08:00:00 AM', function(timer) {
+      console.log(timer);
+  })

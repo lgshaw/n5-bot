70043ab1f8a28ad94089306a1feab421463175c0
Liam Shaw <lgshawry@gmail.com>
2019-02-04 10:38:58 +1100

    raid update

--- n5-bot.js
+++ n5-bot.js
@@ -77,7 +77,7 @@ client.on("message", message =>
                   // },
                   {
                     name: "Raid Progression:",
-                    value: `**BoD:** ${raidProgressCheck(info.progression.raids[41])}`,
+                    value: `**Uldir:** ${raidProgressCheck(info.progression.raids[40])}  **BoD:** ${raidProgressCheck(info.progression.raids[41])}`,
                   },
                   // {
                   //   name: "Raider.IO Mythic+ score:",

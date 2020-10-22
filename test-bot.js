4d194ee0e5eaced168aec1acb4dab1373323b932
Liam Shaw <lgshawry@gmail.com>
2020-10-22 17:07:01 +1100

    token price

--- test-bot.js
+++ test-bot.js
@@ -1,11 +1,15 @@
 import { log } from './util/utilFunctions.js';
 import { getAllTheData } from './util/characterFunctions.js';
+import getWoWTokenPrice from './util/getWoWToken.js';
 
 import config from './config-dev.js';
 const client_id = config.client_id;
 const client_secret = config.client_secret;
 
 
-getAllTheData('shaweaver','caelestrasz', client_id, client_secret)
-.then(response => log(response))
-.catch(error => log(error));
+// getAllTheData('shaweaver','caelestrasz', client_id, client_secret)
+// .then(response => log(response))
+// .catch(error => log(error));
+
+getWoWTokenPrice('us', client_id, client_secret)
+.then(response => console.log(response))
--- util/getWoWToken.js
+++ util/getWoWToken.js
@@ -1,9 +1,20 @@
-const getWoWTokenPrice = ( region, token ) => {
-	return axios(`https://us.api.blizzard.com/data/wow/token/?namespace=dynamic-us&locale=en_US&access_token=${token}`)
-	.then(response => response.data)
+import fetch from 'node-fetch';
+import getAuthToken from './getAuthToken.js';
+
+const getWoWTokenPrice = async ( region, client_id, client_secret ) => {
+	let price;
+	const token = await getAuthToken(client_id, client_secret)
+
+	await fetch(`https://us.api.blizzard.com/data/wow/token/?namespace=dynamic-us&locale=en_US&access_token=${token}`)
+	.then(response => response.json())
+	.then(response => {
+		price = response.price;
+	})
 	.catch(error => {
 			console.log(error);
 	});
+
+	return price;
 }
 
 export default getWoWTokenPrice

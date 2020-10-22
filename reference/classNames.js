5575e9853828c0ac46b7869f331353a897dd5639
Liam Shaw <lgshawry@gmail.com>
2020-10-22 16:42:50 +1100

    color fix

--- reference/classNames.js
+++ reference/classNames.js
@@ -4,39 +4,39 @@ export default [
     "color": "",
   }, {
       "name": "Warrior",
-      "color": '0xc79c6e',
+      "color": 0xc79c6e,
   }, {
     "name": "Paladin",
-    "color": '0xf58cba',
+    "color": 0xf58cba,
   }, {
     "name": "Hunter",
-    "color": '0xABD473',
+    "color": 0xABD473,
   }, {
     "name": "Rogue",
-    "color": '0xFFF569',
+    "color": 0xFFF569,
   }, {
     "name": "Priest",
-    "color": '0xFFFFFF',
+    "color": 0xFFFFFF,
   }, {
     "name": "Death Knight",
-    "color": '0xC41F3B',
+    "color": 0xC41F3B,
   }, {
     "name": "Shaman",
-    "color": '0x0070DE',
+    "color": 0x0070DE,
   }, {
     "name": "Mage",
-    "color": '0x69CCF0',
+    "color": 0x69CCF0,
   }, {
     "name": "Warlock",
-    "color": '0x9482C9',
+    "color": 0x9482C9,
   }, {
     "name": "Monk",
-    "color": '0x00FF96',
+    "color": 0x00FF96,
   }, {
     "name": "Druid",
-    "color": '0xff7d0a',
+    "color": 0xff7d0a,
   }, {
     "name": "Demon Hunter",
-    "color": '0xA330C9',
+    "color": 0xA330C9,
   }
 ];
